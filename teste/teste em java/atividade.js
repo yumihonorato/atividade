bf736ffde62fb828ec61e76a4f0238b94e6740ec
yumihonorato <yngridhonoratouwu@gmail.com>
2026-03-26 17:32:27 +0000

    terminado

--- teste/teste em java/atividade.js
+++ teste/teste em java/atividade.js
@@ -1,29 +1,29 @@
-
-// objelo
+// objeto
 class Carro {
   //atributos:
   constructor(marca,velocidade,cor) {
   this.marca = marca;
-  this.velocidade = velocidade;
-  this.cor=cor;}
+  this.velocidade =velocidade;
+  this.cor=cor;
+    console.log(`seu carro é um ${this.marca} ${this.cor}`)
+  }
   
   //métodos:
   acelerar(){
   if(this.velocidade <= 70){
-    console.log('você está acelerando,sua velocidade é:${this.velocidade}km')
-    this.velocidade+10;}
-  if(this.velocidade >=70 ){
-    console.log('você está muito rapido,sua velocidade é:${velocidade}km')
-    this.velocidade+10}
- if(this.velocidade>=110){
-   console.log('você parado e multado,por isso sua velocidade é:${this.velocidade}')
-    this.velocidadade-velocidade
-  } } }
-let marca='';
-let velocidade='';
-let cor='';
+    this.velocidade +=10;
+    console.log(`voce está acelerando,sua velocidade agora é ${this.velocidade}`)}
+ else if(this.velocidade <=110){
+    this.velocidade+= 10;
+    console.log(`você está muito rapido,sua velocidade é:${this.velocidade}km`)}
+ else{
+   this.velocidade=0
+   console.log(`você parado e multado,por isso sua velocidade é:${this.velocidade}km`)} }
+  }
 
-carro1=new Carro('uno',80,'preto')
+carro1=new Carro('uno',80,'preto') 
 carro1.acelerar()
+
+
 
 
